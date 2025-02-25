b2fd9d2532e757f926047fdca9fb0697a7e7f36f
JaviPedrote <pedrote3.1416@gmail.com>
2025-02-25 14:54:05 +0100

    LinkPOrtfolio

--- src/ui/components/Navbar.tsx
+++ src/ui/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../auth/context/AuthContext';
 import { useContext} from 'react';
 
